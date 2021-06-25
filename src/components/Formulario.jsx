ad05a77df12375f4b2278ac47b92314812d8953c
Alberto Cházaro <shazarerom@gmail.com>
2021-06-25 00:25:38 -0500

    Proyecto criptomonedas

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,101 @@
+import React, {useEffect, useState} from 'react';
+import styled from '@emotion/styled'; 
+import Error from './Error';
+import useMoneda from '../hooks/useMoneda';
+import useCriptomoneda from '../hooks/useCriptomoneda';
+import axios from 'axios';
+
+const Boton = styled.input`
+    margin-top: 20px;
+    font-weight: bold;
+    font-size: 20px;
+    padding: 10px;
+    background-color: #66A2FE;
+    border: none;
+    width: 100%;
+    border-radius: 10px;
+    color: #FFFFFF;
+    transition: background-color .3s ease;
+
+    &:hover {
+        background-color: #326AC0;
+        cursor: pointer;
+    }
+`;
+
+const Formulario = ({guardarMoneda ,guardarCriptomoneda}) => {
+
+    //State del listado de criptomonedas
+    const [listacripto, guardarCriptomonedas] = useState([]);
+    const [error, guardarError] = useState(false);
+
+    const MONEDAS = [
+        {
+            codigo: 'USD',
+            nombre: 'Dolar Estados Unidos'
+        },
+        {
+            codigo: 'MXN',
+            nombre: 'Peso Mexicano'
+        },
+        {
+            codigo: 'EUR',
+            nombre: 'Euro'
+        },
+        {
+            codigo: 'GBP',
+            nombre: 'Libra Esterlina'
+        },
+        {
+            codigo: 'JPY',
+            nombre: 'Yen Japonés'
+        }
+    ]
+
+    //Utilizar useMoneda
+    const [moneda, SelectMonedas] = useMoneda('Elije tu Moneda', '', MONEDAS);
+
+    //Utilizar useCriptomoneda
+    const [criptomoneda, SelectCripto] = useCriptomoneda('Elije tu criptomoneda', '', listacripto);
+
+    //Ejecutar llamado a la API
+    useEffect(() => {
+        const consultarAPI = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+
+            const resultado = await axios.get(url);
+            guardarCriptomonedas(resultado.data.Data);
+        }
+        consultarAPI();
+    }, []);
+
+    //Cuando el usuario hace submit
+    const cotizarMoneda = e => {
+        e.preventDefault();
+
+        //Validar si ambos campos están llenos
+        if (moneda === '' || criptomoneda === '') {
+            guardarError(true);
+            return;
+        }
+
+        //Pasar los datos al componente principal
+        guardarError(false);
+        guardarMoneda(moneda);
+        guardarCriptomoneda(criptomoneda);
+    }
+
+    return (  
+        <form onSubmit={cotizarMoneda}>
+            {error ? <Error mensaje="Selecciona una opción en ambos campos"/> : null}
+            <SelectMonedas />
+            <SelectCripto />
+            <Boton 
+                type="submit"
+                value="Calcular"
+            />
+        </form>
+    );
+}
+ 
+export default Formulario;
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Spinner.css';
+
+const Spinner = () => {
+    return (  
+        <div class="spinner">
+            <div class="cube1"></div>
+            <div class="cube2"></div>
+        </div>
+    );
+}
+ 
+export default Spinner;
+
+    <div class="spinner">
+            <div class="cube1"></div>
+    <div     class="cube2"></div>
+        </div>
